19373f023692ec60b9f85a0da99627505d03f8d0
Dyn-jo <596324727@qq.com>
2020-04-01 21:06:52 +0800

    完善线程池

--- src/main/java/com/dyn/basepro/properties/ThreadPoolProp.java
+++ src/main/java/com/dyn/basepro/properties/ThreadPoolProp.java
@@ -1,17 +1,24 @@
 package com.dyn.basepro.properties;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  *@description: 线程池prop
  *@author: dyn
  *@create: 2020-03-10 21:11
  */
+@Configuration
 @Data
+@PropertySource(value = "classpath:config/common.properties")
 public class ThreadPoolProp {
 
+    @Value("${spring.executor.corePoolSize}")
     private Integer corePoolSize;
 
+    @Value("${spring.executor.maxPoolSize}")
     private Integer maxPoolSize;
 
 }
