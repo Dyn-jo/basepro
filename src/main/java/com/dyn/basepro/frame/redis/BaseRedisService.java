395a7633f42e6964ffefb110a6108cf0fcfa1c10
Dyn-jo <596324727@qq.com>
2020-04-01 21:29:57 +0800

    完善工具类

--- src/main/java/com/dyn/basepro/frame/redis/BaseRedisService.java
+++ src/main/java/com/dyn/basepro/frame/redis/BaseRedisService.java
@@ -25,6 +25,13 @@ public abstract class BaseRedisService {
         }
     }
 
+    /***
+     *@Description: 设置key的值为value
+     *@Param: [key, value]
+     *@return: void
+     *@Author: dyn
+     *@date: 2020/4/1 0001 下午 9:11
+     */
     public void set(String key, String value) {
         Jedis jedis = null;
         try {
@@ -38,17 +45,16 @@ public abstract class BaseRedisService {
     }
 
     public void setEntity(String key, Object o) {
-        Jedis jedis = null;
-        try {
-            jedis = getJedisPool().getResource();
-            jedis.set(key, JSONUtil.parse(o).toString());
-        } catch (Exception e) {
-            throw new BusinessException(ResultCode.SYSTEM_ERROR);
-        } finally {
-            closeResource(jedis);
-        }
+        set(key, JSONUtil.parse(o).toString());
     }
 
+    /***
+     *@Description: 获取指定key的值
+     *@Param: [key]
+     *@return: java.lang.String
+     *@Author: dyn
+     *@date: 2020/4/1 0001 下午 9:11
+     */
     public String get(String key) {
         String value;
         Jedis jedis = null;
@@ -64,21 +70,67 @@ public abstract class BaseRedisService {
     }
 
     public Object getEntity(String key, Object o) {
+
+        try {
+            String value = get(key);
+            JSONObject jsonObject = JSONUtil.parseObj(value);
+            o = JSONUtil.toBean(jsonObject, o.getClass());
+        } catch (Exception e) {
+            throw new BusinessException(ResultCode.SYSTEM_ERROR);
+        }
+        return o;
+    }
+
+    /***
+     *@Description: 删除指定的key, 也可以传入一个包含key的数组
+     *@Param: [keys]
+     *@return: java.lang.Long
+     *@Author: dyn
+     *@date: 2020/4/1 0001 下午 9:15
+     */
+    public Long del(String... keys) {
+        Long value;
         Jedis jedis = null;
         try {
             jedis = getJedisPool().getResource();
-            String value = jedis.get(key);
-            JSONObject jsonObject = JSONUtil.parseObj(value);
-            o = JSONUtil.toBean(jsonObject, o.getClass());
+            value = jedis.del(keys);
         } catch (Exception e) {
             throw new BusinessException(ResultCode.SYSTEM_ERROR);
         } finally {
             closeResource(jedis);
         }
-        return o;
+        return value;
     }
 
-    public void closeResource(Jedis jedis) {
+    /***
+     *@Description: 判断key是否存在
+     *@Param: [key]
+     *@return: java.lang.Boolean
+     *@Author: dyn
+     *@date: 2020/4/1 0001 下午 9:16
+     */
+    public Boolean exists(String key) {
+        Boolean value;
+        Jedis jedis = null;
+        try {
+            jedis = getJedisPool().getResource();
+            value = jedis.exists(key);
+        } catch (Exception e) {
+            throw new BusinessException(ResultCode.SYSTEM_ERROR);
+        } finally {
+            closeResource(jedis);
+        }
+        return value;
+    }
+
+    /***
+     *@Description: 关闭连接
+     *@Param: [jedis]
+     *@return: void
+     *@Author: dyn
+     *@date: 2020/4/1 0001 下午 9:19
+     */
+    private void closeResource(Jedis jedis) {
         try {
             if (jedis != null) {
                 jedis.close();
