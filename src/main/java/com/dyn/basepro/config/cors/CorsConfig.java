4a02306326517f1de1390f25b6ef522141953bff
Dyn-jo <596324727@qq.com>
2020-04-08 22:02:46 +0800

    解决跨域问题

---
+++ src/main/java/com/dyn/basepro/config/cors/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.dyn.basepro.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *@description: 解决跨域问题
+ *@author: dyn
+ *@create: 2020-04-01 21:56
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+
+}
